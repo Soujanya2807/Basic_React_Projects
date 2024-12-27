c3905dbcfb6f59e97367efe6a49b4eb978a6eda9
Soujanya2807 <jayavaramsouji17@gmail.com>
2024-12-27 09:27:58 +0530

    App comp

---
+++ basics/src/Greet.jsx
@@ -0,0 +1,7 @@
+import react from "react";
+
+export default function Greet(props) {
+    return (
+        <div>Hello {props.name}</div>
+    )
+}
---
+++ basics/src/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+    return(
+        <>
+        <h1>Some Heading</h1>
+        <header>Some Header Content</header>
+        </>
+    )
+}
+export default Header;
